7278d021d1243f4418b51095e8b2487e9a5d2e9e
Sidney Sousa <sidney.sousa@ifms.edu.br>
2019-10-18 20:14:19 -0400

    Tabela renderizada

--- Aula03/src/js/index.js
+++ Aula03/src/js/index.js
@@ -2,7 +2,15 @@
  * Nosso BD fake!
  * (in memory DB)
  */
-var contatos = [];
+var contatos = [
+    {
+        nome: 'Sidney',
+        telefone: '888888888',
+        email: 'sidney@email.com',
+        dataNascimento: '29/02/2003'
+    }
+];
+renderizarTabelaContatos();
 
 function salvarContato(event) {
     // Inibe a recarga da página
@@ -43,55 +51,83 @@ function renderizarTabelaContatos() {
          */
         areaListagemContatos.innerHTML = '';
 
-        /*
-        areaListagemContatos.innerHTML = `
-            <table>
-                <thead>
-                    <tr>
-                        <th>Nome</th>
-                        <th>Telefone</th>
-                        <th>E-mail</th>
-                        <th>Data Nasc.</th>
-                    </tr>
-                </thead>
-            </table>
-        `;
-        */
-
         /**
          * Cria a tabela
          */
         let tabela = document.createElement('table');
 
-        /**
-         * Cria o cabeçalho da tabela
-         */
-        let cabecalho = document.createElement('thead');
-        let linhaCabecalho = document.createElement('tr');
-        let colunaNome = document.createElement('th');
-        colunaNome.innerText = 'Nome';
-        let colunaTelefone = document.createElement('th');
-        colunaTelefone.innerText = 'Telefone';
-        let colunaEmail = document.createElement('th');
-        colunaEmail.innerText = 'E-mail';
-        let colunaDataNasc = document.createElement('th');
-        colunaDataNasc.innerText = 'Data Nasc.';
-
-        // Adiciona as colunas na linha do cabeçalho
-        linhaCabecalho.appendChild(colunaNome);
-        linhaCabecalho.appendChild(colunaTelefone);
-        linhaCabecalho.appendChild(colunaEmail);
-        linhaCabecalho.appendChild(colunaDataNasc);
-
-        // Adiciona a linha do cabeçalho no cabeçalho
-        cabecalho.appendChild(linhaCabecalho);
-
+        let cabecalho = criarCabecalhoTabela();
         // Adiciona o cabeçalho dentro da tabela
         tabela.appendChild(cabecalho);
 
+        let corpoTabela = criarCorpoTabela();
+        // Adiciona o corpo da tabela na tabela
+        tabela.appendChild(corpoTabela);
+
         // Adiciona a tabela na área de listagem
         areaListagemContatos.appendChild(tabela);
-
-
     }
+}
+
+function criarCabecalhoTabela() {
+    /**
+    * Cria o cabeçalho da tabela
+    */
+    let cabecalho = document.createElement('thead');
+    let linhaCabecalho = document.createElement('tr');
+    let colunaNome = document.createElement('th');
+    colunaNome.innerText = 'Nome';
+    let colunaTelefone = document.createElement('th');
+    colunaTelefone.innerText = 'Telefone';
+    let colunaEmail = document.createElement('th');
+    colunaEmail.innerText = 'E-mail';
+    let colunaDataNasc = document.createElement('th');
+    colunaDataNasc.innerText = 'Data Nasc.';
+
+    // Adiciona as colunas na linha do cabeçalho
+    linhaCabecalho.appendChild(colunaNome);
+    linhaCabecalho.appendChild(colunaTelefone);
+    linhaCabecalho.appendChild(colunaEmail);
+    linhaCabecalho.appendChild(colunaDataNasc);
+
+    // Adiciona a linha do cabeçalho no cabeçalho
+    cabecalho.appendChild(linhaCabecalho);
+
+    // Retorna o cabeçalho criado
+    return cabecalho;
+}
+
+function criarCorpoTabela() {
+    /**
+     * Cria o corpo da tabela
+     */
+    let corpoTabela = document.createElement('tbody');
+
+    /**
+     * Cria a linhas de contatos
+     */
+    for (let i = 0; i < contatos.length; i++) {
+        /**
+         * Cria uma nova linha no corpo da tabela
+         */
+        let linha = document.createElement('tr');
+
+        let celulaNome = document.createElement('td');
+        celulaNome.innerText = contatos[i].nome;
+        linha.appendChild(celulaNome);
+        let celulaTelefone = document.createElement('td');
+        celulaTelefone.innerText = contatos[i].telefone;
+        linha.appendChild(celulaTelefone);
+        let celulaEmail = document.createElement('td');
+        celulaEmail.innerText = contatos[i].email;
+        linha.appendChild(celulaEmail);
+        let celulaDataNasc = document.createElement('td');
+        celulaDataNasc.innerText = contatos[i].dataNascimento;
+        linha.appendChild(celulaDataNasc);
+
+        // Adiciona a nova linha no corpo da tabela
+        corpoTabela.appendChild(linha);
+    }
+
+    return corpoTabela;
 }
